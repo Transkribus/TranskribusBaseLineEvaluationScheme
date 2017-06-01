808cfa55cb76fd799656f077f2d16df4a59bbbf6
TobiasGruening <tobias.gruening.hro@gmail.com>
2017-06-01 15:01:44 +0200

    Fix of test class.

--- src/test/java/eu/transkribus/baselinemetrictool/Metric_BL_runTest.java
+++ src/test/java/eu/transkribus/baselinemetrictool/Metric_BL_runTest.java
@@ -21,23 +21,9 @@ public class Metric_BL_runTest {
     @Test
     public void testMain() throws Exception {
         
-        String[] args = new String[3];
+        String[] args = new String[2];
             args[0] = "src/test/resources/truth.lst";
             args[1] = "src/test/resources/reco.lst";
-    //        args[0] = "src/test/resources/lineTruth.txt";
-    //        args[1] = "src/test/resources/lineReco1.txt";
-    //        args[1] = "src/test/resources/lineReco2.txt";
-    //        args[1] = "src/test/resources/lineReco3.txt";
-    //        args[1] = "src/test/resources/lineReco4.txt";
-    //        args[1] = "src/test/resources/lineReco5.txt";
-    //        args[1] = "src/test/resources/lineReco6.txt";
-    //        args[1] = "src/test/resources/lineReco7.txt";
-    //        args[1] = "src/test/resources/lineReco8.txt";
-            args[2] = "-p";
-//            args[3] = "-tol";
-//            args[4] = "-i";
-//            args[5] = "src/test/resources/metrEx.png";
-//            args[6] = "-t";
     //        args = ("--help").split(" ");
         
         Metric_BL_run.main(args);
